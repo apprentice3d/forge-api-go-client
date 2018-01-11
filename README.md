905712004b4d69bf49e1ab23df0eb5afe6f28aa1
Denis Grigor <denis.grigor@autodesk.com>
2018-01-11 10:49:15 -0500

    initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+[![Build Status](https://travis-ci.org/apprentice3d/forge-api-go-client.svg?branch=master)](https://travis-ci.org/apprentice3d/forge-api-go-client)
+[![Go Report Card](https://goreportcard.com/badge/github.com/apprentice3d/forge-api-go-client)](https://goreportcard.com/report/github.com/apprentice3d/forge-api-go-client)
+
+# forge-api-go-client
+
+Golang SDK for building Forge based applications
---
+++ doc.go
@@ -0,0 +1,6 @@
+// Package forge is an opinionated SDK for Autodesk Forge services https://developer.autodesk.com/
+//
+// The Forge API for Go provides APIs that developers can use to build Go applications
+// that use Autodesk Forge services, such as Reality Capture API (https://developer.autodesk.com/api/reality-capture-cover-page/),
+// Model Derivative API (https://developer.autodesk.com/api/model-derivative-and-viewer-cover-page/) and many others
+package forge
