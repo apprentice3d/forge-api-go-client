dbf63d0fca59ae9dfb196d85e2517917ee1fffac
Camilla Outer Labs <camillaouterlabs@Camillas-MacBook.local>
2019-12-11 14:22:05 -0800

    Adds GetTopFolders functionality to projects

--- dm/projects.go
+++ dm/projects.go
@@ -9,8 +9,9 @@ type ProjectDetails struct {
 	Details DataDetails `json:"details, omitempty"`
 }
 
+
 // ListBuckets returns a list of all buckets created or associated with Forge secrets used for token creation
-func (api HubAPI) ListProjects(hubKey string) (result HubDetails, err error) {
+func (api HubAPI) ListProjects(hubKey string) (result ProjectDetails, err error) {
 	
 	// TO DO: take in optional arguments for query params: id, ext, page, limit
 	// https://forge.autodesk.com/en/docs/data/v2/reference/http/hubs-hub_id-projects-GET/
@@ -34,11 +35,20 @@ func (api HubAPI) GetProjectDetails(hubKey, projectKey string) (result ProjectDe
 	return getProjectDetails(path, hubKey, projectKey, bearer.AccessToken)
 }
 
+func (api HubAPI) GetTopFolders(hubKey, projectKey string) (result ProjectDetails, err error) {
+	bearer, err := api.Authenticate("data:read")
+	if err != nil {
+		return
+	}
+	path := api.Host + api.HubAPIPath
+
+	return getTopFolders(path, hubKey, projectKey, bearer.AccessToken)
+}
 
 /*
  *	SUPPORT FUNCTIONS
  */
-func listProjects(path, hubKey, id, extension, page, limit string, token string) (result HubDetails, err error) {
+func listProjects(path, hubKey, id, extension, page, limit string, token string) (result ProjectDetails, err error) {
 	task := http.Client{}
 
 	req, err := http.NewRequest("GET",
@@ -90,6 +100,37 @@ func getProjectDetails(path, hubKey, projectKey, token string) (result ProjectDe
 
 	req, err := http.NewRequest("GET",
 		path+"/"+hubKey+"/projects/"+projectKey,
+		nil,
+	)
+
+	if err != nil {
+		return
+	}
+
+	req.Header.Set("Authorization", "Bearer "+token)
+	response, err := task.Do(req)
+	if err != nil {
+		return
+	}
+	defer response.Body.Close()
+
+  	decoder := json.NewDecoder(response.Body)
+	if response.StatusCode != http.StatusOK {
+    	err = &ErrorResult{StatusCode:response.StatusCode}
+    	decoder.Decode(err)
+		return
+	}
+
+	err = decoder.Decode(&result)
+
+	return
+}
+
+func getTopFolders(path, hubKey, projectKey, token string) (result ProjectDetails, err error) {
+	task := http.Client{}
+
+	req, err := http.NewRequest("GET",
+		path+"/"+hubKey+"/projects/"+projectKey+"/topFolders",
 		nil,
 	)
 
--- dm/projects_test.go
+++ dm/projects_test.go
@@ -64,3 +64,32 @@ func TestProjectAPI_GetProjectDetails(t *testing.T) {
 }
 
 
+func TestProjectAPI_GetTopFolders(t *testing.T) {
+
+	// prepare the credentials
+	clientID := os.Getenv("FORGE_CLIENT_ID")
+	clientSecret := os.Getenv("FORGE_CLIENT_SECRET")
+
+	hubAPI := dm.NewHubAPIWithCredentials(clientID, clientSecret)
+
+	testHubKey := os.Getenv("BIM_360_TEST_ACCOUNT_HUBKEY")
+	testProjectKey := os.Getenv("BIM_360_TEST_ACCOUNT_PROJECTKEY")
+
+	t.Run("List all projects under a given hub", func(t *testing.T) {
+		_, err := hubAPI.GetTopFolders(testHubKey, testProjectKey)
+		
+		if err != nil {
+			t.Fatalf("Failed to get project details: %s\n", err.Error())
+		}
+	})
+
+	t.Run("List all projects under non-existent hub (should fail)", func(t *testing.T) {
+		_, err := hubAPI.GetTopFolders(testHubKey, testProjectKey + "30091981")
+
+		if err == nil {
+			t.Fatalf("Should fail getting getting projects for non-existing hub\n")
+		}
+	})
+}
+
+
