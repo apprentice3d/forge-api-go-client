f435412fba015f4967302bfee3ac9eebba93c9e6
Camilla Outer Labs <camillaouterlabs@Camillas-MacBook.local>
2019-12-11 14:43:55 -0800

    adds Tip and Versions functionality to items

--- dm/items_test.go
+++ dm/items_test.go
@@ -24,3 +24,43 @@ func TestFolderAPI_GetItemDetails(t *testing.T) {
 		}
 	})
 }
+
+func TestFolderAPI_GetItemTip(t *testing.T) {
+
+	// prepare the credentials
+	clientID := os.Getenv("FORGE_CLIENT_ID")
+	clientSecret := os.Getenv("FORGE_CLIENT_SECRET")
+
+	folderAPI := NewFolderAPIWithCredentials(clientID, clientSecret)
+
+	testProjectKey := os.Getenv("BIM_360_TEST_ACCOUNT_PROJECTKEY")
+	testItemKey := os.Getenv("BIM_360_TEST_ACCOUNT_ITEMKEY")
+
+	t.Run("List item details", func(t *testing.T) {
+		_, err := folderAPI.GetItemTip(testProjectKey, testItemKey)
+
+		if err != nil {
+			t.Fatalf("Failed to get item details: %s\n", err.Error())
+		}
+	})
+}
+
+func TestFolderAPI_GetItemVersions(t *testing.T) {
+
+	// prepare the credentials
+	clientID := os.Getenv("FORGE_CLIENT_ID")
+	clientSecret := os.Getenv("FORGE_CLIENT_SECRET")
+
+	folderAPI := NewFolderAPIWithCredentials(clientID, clientSecret)
+
+	testProjectKey := os.Getenv("BIM_360_TEST_ACCOUNT_PROJECTKEY")
+	testItemKey := os.Getenv("BIM_360_TEST_ACCOUNT_ITEMKEY")
+
+	t.Run("List item details", func(t *testing.T) {
+		_, err := folderAPI.GetItemVersions(testProjectKey, testItemKey)
+
+		if err != nil {
+			t.Fatalf("Failed to get item details: %s\n", err.Error())
+		}
+	})
+}
