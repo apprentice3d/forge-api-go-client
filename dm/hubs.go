eacd2733bb1a73857b0de20225b64dd499d59ebb
Camilla Mahon <camilla@outerlabs.io>
2020-03-05 16:27:09 -0800

    add getHubs to list hubs for a given user

--- dm/hubs.go
+++ dm/hubs.go
@@ -23,6 +23,16 @@ func NewHubAPIWithCredentials(ClientID string, ClientSecret string) HubAPI {
 	}
 }
 
+func (api HubAPI) GetHubs() (result ForgeResponseArray, err error) {
+	bearer, err := api.Authenticate("data:read")
+	if err != nil {
+		return
+	}
+	path := api.Host + api.HubAPIPath
+
+	return getHubs(path, bearer.AccessToken)
+}
+
 func (api HubAPI) GetHubDetails(hubKey string) (result ForgeResponseObject, err error) {
 	bearer, err := api.Authenticate("data:read")
 	if err != nil {
@@ -33,9 +43,43 @@ func (api HubAPI) GetHubDetails(hubKey string) (result ForgeResponseObject, err 
 	return getHubDetails(path, hubKey, bearer.AccessToken)
 }
 
+
 /*
  *	SUPPORT FUNCTIONS
  */
+
+func getHubs(path, token string) (result ForgeResponseArray, err error) {
+	task := http.Client{}
+
+	req, err := http.NewRequest("GET",
+		path,
+		nil,
+	)
+
+	if err != nil {
+		return
+	}
+
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	response, err := task.Do(req)
+	if err != nil {
+		return
+	}
+	defer response.Body.Close()
+	
+	decoder := json.NewDecoder(response.Body)
+		if response.StatusCode != http.StatusOK {
+			err = &ErrorResult{StatusCode:response.StatusCode}
+			decoder.Decode(err)
+				return
+		}
+	
+	err = decoder.Decode(&result)
+
+	return
+}
+
 func getHubDetails(path, hubKey, token string) (result ForgeResponseObject, err error) {
 	task := http.Client{}
 
