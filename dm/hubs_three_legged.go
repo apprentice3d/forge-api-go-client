6ee613caae01ec97032803bb50a15a802ec23f74
Paul M Fox <iam@yourheropaul.com>
2020-03-12 14:50:41 +0000

    introduced async token refresher

--- dm/hubs_three_legged.go
+++ dm/hubs_three_legged.go
@@ -1,99 +1,66 @@
 package dm
 
-import (
-	// "fmt"
-	"time"
-	"github.com/outer-labs/forge-api-go-client/oauth"
-)
+import "github.com/outer-labs/forge-api-go-client/oauth"
 
 type HubAPI3L struct {
-	Auth        oauth.ThreeLeggedAuth
-	BearerToken *oauth.Bearer
-	HubAPIPath  string
-	TokenExpireTime time.Time
+	Auth       oauth.ThreeLeggedAuth
+	Token      TokenRefresher
+	HubAPIPath string
 }
 
 func NewHubAPI3LWithCredentials(
 	auth oauth.ThreeLeggedAuth,
-	bearer *oauth.Bearer,
+	token TokenRefresher,
 ) *HubAPI3L {
 	return &HubAPI3L{
-		Auth:        auth,
-		BearerToken: bearer,
-		HubAPIPath:  "/project/v1/hubs",
-		TokenExpireTime: time.Now(),
+		Auth:       auth,
+		Token:      token,
+		HubAPIPath: "/project/v1/hubs",
 	}
 }
 
 // Hub functions for use with 3legged authentication
 func (a *HubAPI3L) GetHubsThreeLegged() (result ForgeResponseArray, err error) {
-	if err = a.refreshTokenIfRequired(); err != nil {
+	if err = a.Token.RefreshTokenIfRequired(a.Auth); err != nil {
 		return
 	}
 
 	path := a.Auth.Host + a.HubAPIPath
-	return getHubs(path, a.BearerToken.AccessToken)
+	return getHubs(path, a.Token.Bearer().AccessToken)
 }
 
 func (a *HubAPI3L) GetHubDetailsThreeLegged(hubKey string) (result ForgeResponseObject, err error) {
-	if err = a.refreshTokenIfRequired(); err != nil {
+	if err = a.Token.RefreshTokenIfRequired(a.Auth); err != nil {
 		return
 	}
 
 	path := a.Auth.Host + a.HubAPIPath
-	return getHubDetails(path, hubKey, a.BearerToken.AccessToken)
+	return getHubDetails(path, hubKey, a.Token.Bearer().AccessToken)
 }
 
 func (a *HubAPI3L) ListProjectsThreeLegged(hubKey string) (result ForgeResponseArray, err error) {
-	if err = a.refreshTokenIfRequired(); err != nil {
+	if err = a.Token.RefreshTokenIfRequired(a.Auth); err != nil {
 		return
 	}
 
 	path := a.Auth.Host + a.HubAPIPath
-	return listProjects(path, hubKey, "", "", "", "", a.BearerToken.AccessToken)
+	return listProjects(path, hubKey, "", "", "", "", a.Token.Bearer().AccessToken)
 }
 
 func (a *HubAPI3L) GetProjectDetailsThreeLegged(hubKey, projectKey string) (result ForgeResponseObject, err error) {
-	if err = a.refreshTokenIfRequired(); err != nil {
+	if err = a.Token.RefreshTokenIfRequired(a.Auth); err != nil {
 		return
 	}
 
 	path := a.Auth.Host + a.HubAPIPath
-	return getProjectDetails(path, hubKey, projectKey, a.BearerToken.AccessToken)
+	return getProjectDetails(path, hubKey, projectKey, a.Token.Bearer().AccessToken)
 }
 
 func (a *HubAPI3L) GetTopFoldersThreeLegged(hubKey, projectKey string) (result ForgeResponseArray, err error) {
-	if err = a.refreshTokenIfRequired(); err != nil {
+	if err = a.Token.RefreshTokenIfRequired(a.Auth); err != nil {
 		return
 	}
 
 	path := a.Auth.Host + a.HubAPIPath
-	return getTopFolders(path, hubKey, projectKey, a.BearerToken.AccessToken)
-}
-
-func (a *HubAPI3L) refreshTokenIfRequired() error {
-	
-	// Check if token has expired
-	now := time.Now()
-	expiryTime := a.TokenExpireTime
-	if now.Before(expiryTime){
-		return nil
-	}
-
-	refreshedBearer, err := a.Auth.RefreshToken(a.BearerToken.RefreshToken, "data:read")
-	if err != nil {
-		return err
-	}
-
-	// Refresh "now" and add new token expiration time to API struct along with new credentials
-	now = time.Now()
-	newExpiryTime := now.Add(time.Second * time.Duration(refreshedBearer.ExpiresIn))
-	a.TokenExpireTime = newExpiryTime
-
-	a.BearerToken.AccessToken = refreshedBearer.AccessToken
-	a.BearerToken.ExpiresIn = refreshedBearer.ExpiresIn
-	a.BearerToken.RefreshToken = refreshedBearer.RefreshToken
-	a.BearerToken.TokenType = refreshedBearer.TokenType
-
-	return nil
+	return getTopFolders(path, hubKey, projectKey, a.Token.Bearer().AccessToken)
 }
