9f8587d2f4bd3ea7665d54393ba0c475e362be2e
Wolfgang Weh <wolfgang.weh@neanex.com>
2022-09-01 22:37:11 +0200

    Get number of download/upload parts

--- dm/object.go
+++ dm/object.go
@@ -4,9 +4,21 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"strconv"
+)
+
+const (
+	// megaByte is 1048576 bytes
+	megaByte = 1 << 20
+)
+
+var (
+	// defaultSize is the default size of download/upload chunks.
+	defaultSize = int64(100 * megaByte)
 )
 
 // UploadObject adds to specified bucket the given data (can originate from a multipart-form or direct file read).
@@ -17,6 +29,33 @@ func (api BucketAPI) UploadObject(bucketKey, objectName, fileToUpload string) (r
 		return
 	}
 	path := api.Authenticator.GetHostPath() + api.BucketAPIPath
+
+	// Instructions for the S3 update:
+	// https://forge.autodesk.com/blog/data-management-oss-object-storage-service-migrating-direct-s3-approach
+	/*
+			Direct-to-S3 approach for Data Management OSS
+			To upload and download files, applications must generate a signed URL, then upload or download the binary. Here are the steps (pseudo code):
+
+			Upload
+			========
+
+			1. Calculate the number of parts of the file to upload
+				Note: Each uploaded part except for the last one must be at least 5MB (1024 * 5)
+
+			2. Generate up to 25 URLs for uploading specific parts of the file using the
+			   GET buckets/:bucketKey/objects/:objectKey/signeds3upload?firstPart=<index of first part>&parts=<number of parts>
+		       endpoint.
+				a) The part numbers start with 1
+				b) For example, to generate upload URLs for parts 10 through 15, set firstPart to 10 and parts to 6
+				c) This endpoint also returns an uploadKey that is used later to request additional URLs or to finalize the upload
+
+			3. Upload remaining parts of the file to their corresponding upload URLs
+				a) Consider retrying (for example, with an exponential backoff) individual uploads when the response code is 100-199, 429, or 500-599
+				b) If the response code is 403, the upload URLs have expired; go back to step #2
+				c) If you have used up all the upload URLs and there are still parts that must be uploaded, go back to step #2
+
+			4. Finalize the upload using the POST buckets/:bucketKey/objects/:objectKey/signeds3upload endpoint, using the uploadKey value from step #2
+	*/
 
 	// Step 1, generate signed S3 upload url(s)
 
@@ -111,6 +150,36 @@ func getSignedUploadUrls(path, bucketKey, objectName, fileToUpload string, minut
 	// 1 - determine the required number of parts
 	// In the examples, typically a chunk size of 5 or 10 MB is used.
 	// In the old API, the boundary for multipart uploads was 100 MB.
+
+	fileInfo, err := os.Stat(fileToUpload)
+	if err != nil {
+		return
+	}
+
+	parts, err := getNumberOfParts(fileInfo.Size())
+	if err != nil {
+		return
+	}
+
+	// request the signed urls
+
+	return
+}
+
+// getNumberOfParts calculates the number of upload parts
+func getNumberOfParts(fileSize int64) (parts int64, err error) {
+
+	if fileSize <= defaultSize {
+		// use just one part
+		parts = 1
+		return
+	}
+
+	parts = fileSize / defaultSize
+	if parts > 25 {
+		err = fmt.Errorf("file is too large to upload (%d byte)", fileSize)
+		return
+	}
 
 	return
 }
